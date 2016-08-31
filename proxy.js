c951ffbfc0ed7103fc2783541208f99b68e55f78
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-08-31 14:41:24 -0500

    changed route for redirect.

--- proxy.js
+++ proxy.js
@@ -2,10 +2,21 @@ var app     = require('express')();
 var proxy   = require('express-http-proxy');
 var url     = require('url');
 
-app.use('/', proxy(process.env.AUTH_URL, {
+app.use('/api/v1/auth', proxy(process.env.AUTH_URL, {
   forwardPath: function(req, res) {
     return '/connect/google';
   }
 }));
+
+// successful return from auth
+app.use('/api/v1/success', function(req, res) {
+  // send to task service
+  res.send('SUCCESS!');
+});
+
+app.use('/login', function(req, res) {
+  // you failed
+  res.send('LOGIN FAILED!');
+});
 
 app.listen(process.env.PORT || 4532);
